590c30c55ef1bc93cbcdaf5616ddbc26a52e70e6
arf-rumo <arf.rumo@kakaocorp.com>
2018-09-17 18:14:36 +0900

    d

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,9 @@
         <v-card-title class="teal">
           <strong class="subheading">하트</strong>
           <v-spacer></v-spacer>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
-            <v-icon size="24px">{{ icon }}</v-icon>
-          </v-btn>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon><v-icon size="24px">{{ icon }}</v-icon></v-btn>
         </v-card-title>
-        <v-card-actions class="grey darken-3 justify-center">
-          &copy;2018 — <strong>Hart</strong>
-        </v-card-actions>
+        <v-card-actions class="grey darken-3 justify-center">&copy;2018 — <strong>Hart</strong></v-card-actions>
       </v-card>
     </v-footer>
   </div>
