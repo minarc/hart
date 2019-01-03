81b5e3a546dd7f38e4e4739f7d042c3aa8559ef8
arf-rumo <arf.rumo@kakaocorp.com>
2019-01-04 02:22:42 +0900

    travis ci

--- src/components/Today.vue
+++ src/components/Today.vue
@@ -1,36 +1,43 @@
 <template>
-<v-container grid-list-md>
-        <v-layout row wrap>
-          <v-flex xs6>
-          <v-layout column warp>
-            <v-flex xs6 v-for="n in 4" :key="n">
-              <v-card color="cyan darken-2" class="white--text">
-                <v-layout>
-                  <v-flex xs4>
-                    <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg" height="125px"></v-img>
-                  </v-flex>
-                  <v-flex xs8>
-                    <v-card-title class="text-sm-left">
-                      <div>
-                        <div class="headline">Supermodel</div>
-                        <div>Foster the People</div>
-                      </div>
-                    </v-card-title>
-                  </v-flex>
-                </v-layout>
-                <v-divider light></v-divider>
-                <v-card-actions class="pa-2">
-                  <v-rating v-model="rating" color="teal accent-4" dense readonly half-increments background-color="teal lighten-4"></v-rating>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-         <v-flex xs6>
-            <v-layout column warp>
-            <v-flex xs6  v-for="n in 4" :key="n">
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs6>
+        <v-layout column warp>
+          <v-flex xs6 v-for="n in 4" :key="n">
+            <v-card color="cyan darken-2" class="white--text">
+              <v-layout>
+                <v-flex xs4>
+                  <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg" height="125px"></v-img>
+                </v-flex>
+                <v-flex xs8>
+                  <v-card-title class="text-sm-left">
+                    <div>
+                      <div class="headline">Supermodel</div>
+                      <div>Foster the People</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions class="pa-2">
+                <v-rating
+                  v-model="rating"
+                  color="teal accent-4"
+                  dense
+                  readonly
+                  half-increments
+                  background-color="teal lighten-4"
+                ></v-rating>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex xs6>
+        <v-layout column warp>
+          <v-flex xs6 v-for="n in 4" :key="n">
             <v-card color="purple" class="white--text">
-              <v-layout >
+              <v-layout>
                 <v-flex xs8>
                   <v-card-title>
                     <div>
@@ -44,17 +51,23 @@
                 </v-flex>
               </v-layout>
               <v-divider light></v-divider>
-              <v-card-actions class="pa-2">
-                confidence
+              <v-card-actions class="pa-2">confidence
                 <v-spacer></v-spacer>
-                <v-rating v-model="rating" color="teal accent-4" dense readonly half-increments background-color="teal lighten-4"></v-rating>
+                <v-rating
+                  v-model="rating"
+                  color="teal accent-4"
+                  dense
+                  readonly
+                  half-increments
+                  background-color="teal lighten-4"
+                ></v-rating>
               </v-card-actions>
             </v-card>
-            </v-flex>
-            </v-layout>
-         </v-flex>
+          </v-flex>
         </v-layout>
-      </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -65,10 +78,6 @@ export default {
   mounted () {},
   methods: {
     list () {
-      // axios.get('/api/today').then(response => {
-      // }).catch(error => {
-      //   console.log(error)
-      // })
     }
   }
 }
