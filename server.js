89077af8c8e1aec89c5b9381244ca45cb3839060
rumo <arfrumo@gmail.com>
2019-01-05 00:59:10 +0900

    logging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const serveStatic = require("serve-static")
 const path = require('path')
+const morgan = require('morgan')
 const middleware = require('http-proxy-middleware')
 
 app = express()
@@ -13,6 +14,7 @@ const options = {
 	},
 	router: {}
 }
+app.use(morgan('combined'))
 app.use(serveStatic(path.join(__dirname, 'dist')), middleware(options))
 
 const port = process.env.PORT || 8080
